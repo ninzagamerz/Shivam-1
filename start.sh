00b4281eb961b112926077f4fe1f90d735274f63
funtoonsmultimediaofficial <funtoonsmultimedia@gmail.com>
2024-08-15 14:41:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sahildesai07/TheMovieProviderBot.git /TheMovieProviderBot
+  git clone https://github.com/funtoonsmultimediaofficial/Shivam.git /Shivam
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /Shivam
 fi
-cd /TheMovieProviderBot
+cd /Shivam
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting Shivam...."
 python3 bot.py
